857fa53d1f765663f79e305b18edbde72f89ce60
acrusedev <nominos09@gmail.com>
2025-12-11 22:09:16 +0100

    add: save mnist binaries to tmp folder

--- examples/mnist.rs
+++ examples/mnist.rs
@@ -1,3 +1,4 @@
+use std::{fs::File, io::Write};
 use tempdir::TempDir; // save mnist files to temp directory
 
 // https://raw.githubusercontent.com/fgnt/mnist/master/train-images-idx3-ubyte.gz
@@ -10,15 +11,21 @@ fn main() {
 }
 
 fn download_mnist() {
-    let mnist_urls: Vec<&str> = vec![
+    let mnist_urls = vec![
         "https://raw.githubusercontent.com/fgnt/mnist/master/train-images-idx3-ubyte.gz",
         "https://raw.githubusercontent.com/fgnt/mnist/master/train-labels-idx1-ubyte.gz",
         "https://raw.githubusercontent.com/fgnt/mnist/master/t10k-images-idx3-ubyte.gz",
         "https://raw.githubusercontent.com/fgnt/mnist/master/t10k-labels-idx1-ubyte.gz",
     ];
 
+    let tmp_dir = TempDir::new("mnist").unwrap();
+    println!("Temp dir: {:?}", tmp_dir.path());
+
     for url in mnist_urls {
         let resp = reqwest::blocking::get(url).unwrap();
-        assert_eq!(resp.status().as_u16(), 200);
+        let file_path = tmp_dir.path().join(url.split('/').last().unwrap());
+        let mut file = File::create(&file_path).unwrap();
+        file.write_all(&resp.bytes().unwrap()).unwrap();
+        println!("Saved: {:?}", file_path);
     }
 }
