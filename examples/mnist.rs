aa5dbb8f1c29f4cbbd2c2501770410825c152957
acrusedev <nominos09@gmail.com>
2025-12-16 15:39:06 +0100

    add debug

--- examples/mnist.rs
+++ examples/mnist.rs
@@ -1,31 +1,27 @@
-use std::{fs::File, io::Write};
-use tempdir::TempDir; // save mnist files to temp directory
-
-// https://raw.githubusercontent.com/fgnt/mnist/master/train-images-idx3-ubyte.gz
-// https://raw.githubusercontent.com/fgnt/mnist/master/train-labels-idx1-ubyte.gz
-// https://raw.githubusercontent.com/fgnt/mnist/master/t10k-images-idx3-ubyte.gz
-// https://raw.githubusercontent.com/fgnt/mnist/master/t10k-labels-idx1-ubyte.gz
+use autograd::nn::datasets::mnist;
+use autograd::type_name;
+use autograd::types::tensor_array::ToTensorArray;
+use autograd::tensor::Tensor;
 
 fn main() {
-    download_mnist();
+    let mnist = mnist();
+    let x_train: &[u8] = &mnist[0][16..];
+    let y_train: &[u8] = &mnist[1][8..];
+    let x_test: &[u8] = &mnist[2][16..];
+    let y_test: &[u8] = &mnist[3][8..];
+    type_name!(x_train);
+    type_name!(y_train);
+    type_name!(x_test);
+    type_name!(y_test);
+    
+    let x_train_tensor_array = x_train.to_tensor_array();
+    let x_train_tensor = Tensor::new(x_train);
+    type_name!(x_train_tensor_array);
+    type_name!(x_train_tensor.data);
+
+    // let x_train_tensor = Tensor::from_slice(x_train);
+    // let y_train_tensor = Tensor::from_slice(y_train);
+    // let x_test_tensor = Tensor::from_slice(x_test);
+    // let y_test_tensor = Tensor::from_slice(y_test);
 }
 
-fn download_mnist() {
-    let mnist_urls = vec![
-        "https://raw.githubusercontent.com/fgnt/mnist/master/train-images-idx3-ubyte.gz",
-        "https://raw.githubusercontent.com/fgnt/mnist/master/train-labels-idx1-ubyte.gz",
-        "https://raw.githubusercontent.com/fgnt/mnist/master/t10k-images-idx3-ubyte.gz",
-        "https://raw.githubusercontent.com/fgnt/mnist/master/t10k-labels-idx1-ubyte.gz",
-    ];
-
-    let tmp_dir = TempDir::new("mnist").unwrap();
-    println!("Temp dir: {:?}", tmp_dir.path());
-
-    for url in mnist_urls {
-        let resp = reqwest::blocking::get(url).unwrap();
-        let file_path = tmp_dir.path().join(url.split('/').last().unwrap());
-        let mut file = File::create(&file_path).unwrap();
-        file.write_all(&resp.bytes().unwrap()).unwrap();
-        println!("Saved: {:?}", file_path);
-    }
-}
