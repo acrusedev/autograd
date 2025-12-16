50a449144bbc87ecf97c20efd472743d8abc0b48
acrusedev <nominos09@gmail.com>
2025-12-16 11:00:32 +0100

    add: tensor_array! macro

--- src/types/tensor_array.rs
+++ src/types/tensor_array.rs
@@ -1,18 +1,14 @@
-struct TensorArray {
-    // Represents a multi-dimensional, homogenous array of fixed items.
-    shape: Vec<usize>,
+#[derive(Debug)]
+pub enum TensorArray<T> {
+    Value(T),
+    List(Vec<TensorArray<T>>),
 }
 
-impl TensorArray {
-    fn new(shape: Vec<usize>) -> Self {
-        Self { shape }
-    }
-
-    fn zeros(shape: Vec<usize>) -> Self {
-        Self { shape }
-    }
-
-    fn empty(shape: Vec<usize>) -> Self {
-        Self { shape }
-    }
+#[macro_export]
+macro_rules! tensor_array {
+    ([]) => { $create::types::tensor_array::TensorArray::List(Vec::new()) };
+    ($val: expr) => { $crate::types::tensor_array::TensorArray::Value($val) };
+    ([$($inner:tt),* $(,)?]) => {
+        $create::types::tensor_array::TensorArray::List(vec![$(tensor_array!($inner)),*])
+    };
 }
