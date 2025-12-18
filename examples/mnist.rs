0aa14dc93bb3813be5225d413171fe613c36dbae
acrusedev <nominos09@gmail.com>
2025-12-18 22:03:30 +0100

    todo : infer tensor array shape

--- examples/mnist.rs
+++ examples/mnist.rs
@@ -1,7 +1,7 @@
 use autograd::nn::datasets::mnist;
-use autograd::type_name;
-use autograd::types::tensor_array::ToTensorArray;
 use autograd::tensor::Tensor;
+use autograd::types::tensor_array::ToTensorArray;
+use autograd::{tensor_array, type_name};
 
 fn main() {
     let mnist = mnist();
@@ -13,15 +13,22 @@ fn main() {
     type_name!(y_train);
     type_name!(x_test);
     type_name!(y_test);
-    
+
     let x_train_tensor_array = x_train.to_tensor_array();
-    let x_train_tensor = Tensor::new(x_train);
+    // let x_train_tensor = Tensor::new(x_train);
     type_name!(x_train_tensor_array);
+    // type_name!(x_train_tensor.data);
+
+    let x_train_tensor = Tensor::from_slice(x_train);
+    let y_train_tensor = Tensor::from_slice(y_train);
+    let x_test_tensor = Tensor::from_slice(x_test);
+    let y_test_tensor = Tensor::from_slice(y_test);
     type_name!(x_train_tensor.data);
+    type_name!(y_train_tensor.data);
+    type_name!(x_test_tensor.data);
+    type_name!(y_test_tensor.data);
 
-    // let x_train_tensor = Tensor::from_slice(x_train);
-    // let y_train_tensor = Tensor::from_slice(y_train);
-    // let x_test_tensor = Tensor::from_slice(x_test);
-    // let y_test_tensor = Tensor::from_slice(y_test);
+    let x = tensor_array!([[1, 2, 3], [4, 5, 6]]);
+    println!("x: {:?}", x); //x: List([List([Value(1), Value(2), Value(3)]), List([Value(4), Value(5), Value(6)])])
+    type_name!(x);
 }
-
--- src/types/tensor_array.rs
+++ src/types/tensor_array.rs
@@ -7,8 +7,8 @@ pub enum TensorArray<T> {
 /// Create a tensor array from an iterable.
 #[macro_export]
 macro_rules! tensor_array {
-    ([]) => { $crateeii:tensor_array::TensorArray::List(Vec::new()) };
-    ([$($inner:tt),* $(,)?]) => 
+    ([]) => { $crate::types::tensor_array::TensorArray::List(Vec::new()) };
+    ([$($inner:tt),* $(,)?]) =>
     {
         $crate::types::tensor_array::TensorArray::List(vec![$(tensor_array!($inner)),*])
     };
@@ -28,7 +28,7 @@ impl<T> TensorArray<T> {
     }
 }
 
-/// Trait to convert iterables into tensor arrays. Helps remove boilerplate code in exposed api.
+/// Trait to convert iterables into tensor arrays.
 pub trait ToTensorArray<T: Clone> {
     fn to_tensor_array(&self) -> Vec<T>;
 }
@@ -37,5 +37,17 @@ pub trait ToTensorArray<T: Clone> {
 impl<T: Clone> ToTensorArray<T> for &[T] {
     fn to_tensor_array(&self) -> Vec<T> {
         self.to_vec()
+    }
+}
+
+/// Trait to infer the shape of an iterable.
+pub trait InferShape<T> {
+    fn infer_shape(&self) -> Vec<usize>;
+}
+
+impl<T> InferShape<T> for &[T] {
+    fn infer_shape(&self) -> Vec<usize> {
+        println!("self len: {:?}", self.len());
+        vec![self.len(), 1]
     }
 }
