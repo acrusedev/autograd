950f0fc5e6af1771dbc0e3c573c4036704e71f71
acrusedev <nominos09@gmail.com>
2026-01-04 15:58:48 +0100

    fix: init Tensor without a shape

--- autograd/tensor.py
+++ autograd/tensor.py
@@ -1,19 +1,19 @@
 from typing import Iterable, Optional
 
 def _infer_shape(data: Iterable) -> Iterable:
-    pass
+    return []
 
 def _check_shape(shape: Iterable, data: Iterable) -> bool:
     """
     Check if provided shape is compatible with existing data.
     All data's elements must fit into the provided shape.
     """
-    pass
+    return True
 
 class Tensor:
     def __init__(self, data: Iterable, shape: Optional[Iterable] = None):
         self.data = data
-        self.shape = shape if _check_shape(shape, data) and shape is not None else _infer_shape(data)
+        self.shape = shape if shape and _check_shape(shape, data) else _infer_shape(data)
 
     def reshape(self, shape: Iterable):
         self.shape = shape if _check_shape(shape, self.data) else self.shape
