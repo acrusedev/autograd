666591cd0b89c71884f0b697a98d2e03336bba77
acrusedev <nominos09@gmail.com>
2026-01-07 11:15:15 +0100

    update mnist

--- autograd/datasets/__init__.py
+++ autograd/datasets/__init__.py
@@ -1,42 +1,6 @@
-from autograd.helpers import fetch
 from autograd.tensor import Tensor
 
 def mnist():
-    import requests
     base_url = "https://raw.githubusercontent.com/fgnt/mnist/master/"
-    files = [
-        "train-images-idx3-ubyte.gz",
-        "train-labels-idx1-ubyte.gz",
-        "t10k-images-idx3-ubyte.gz",
-        "t10k-labels-idx1-ubyte.gz",
-    ]
-
-
-    def _mnist(file): return Tensor.from_url(base_url+file)
-
-    contents = []
-
-    for file in files:
-        contents.append(_mnist(file))
-        
-    return contents
-
-
-"""
-pub fn mnist() -> Vec<Vec<u8>> {
-    let base_url = String::from("https://raw.githubusercontent.com/fgnt/mnist/master/");
-    let urls = vec![
-        base_url.clone() + "train-images-idx3-ubyte.gz",
-        base_url.clone() + "train-labels-idx1-ubyte.gz",
-        base_url.clone() + "t10k-images-idx3-ubyte.gz",
-        base_url.clone() + "t10k-labels-idx1-ubyte.gz",
-    ];
-    let mut arr_to_ret: Vec<Vec<u8>> = Vec::new();
-    for url in urls {
-        let resp = reqwest::blocking::get(url).unwrap();
-        let bytes = resp.bytes().unwrap();
-        arr_to_ret.push(bytes.to_vec());
-    }
-    arr_to_ret
-}
-"""
+    def _mnist(file) -> Tensor: return Tensor.from_url(base_url+file)
+    return _mnist("train-images-idx3-ubyte.gz")[0x10:].reshape((-1,1,28,28)), _mnist("train-labels-idx1-ubyte.gz")[8:], _mnist("t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1,1,28,28)), _mnist("t10k-labels-idx1-ubyte.gz")[8:]
--- autograd/examples/mnist.py
+++ autograd/examples/mnist.py
@@ -2,5 +2,4 @@ from autograd.datasets import mnist
 
 train_images, train_labels, test_images, test_labels = mnist()
 
-print(type(train_images))
-print(train_images)
+print(train_images, train_labels, test_images, test_labels)
