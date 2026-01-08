5f84249103f82019cf786050cb61ac4a856b7d37
acrusedev <nominos09@gmail.com>
2026-01-08 15:57:16 +0100

    add: dtypes

---
+++ autograd/dtypes.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import Literal
+
+FmtStr = Literal['f', 'b'] # later we will extend this, for now we have support for float32 and signed int8
+@dataclass(frozen=True) # immutable class object
+class DType:
+    """
+    priority: needed to update tensor's data type when operating on tensors with different dtypes
+    name: easy to read for human 
+    bit_size: bit size of a single dtype element in memory
+    fmt: needed for Python ABI, to parse Rust's formats
+    """
+    priority: int
+    name: str
+    bit_size: int
+    fmt: FmtStr
+    def __init__(self):
+        pass
+
+class dtypes:
+    pass
