6d8eacaaf81d6e10566daf5c5989be460b0c3fd7
acrusedev <nominos09@gmail.com>
2026-01-12 21:05:33 +0100

    refactor: pass dtype to mnist

--- autograd/datasets/__init__.py
+++ autograd/datasets/__init__.py
@@ -1,6 +1,7 @@
+from autograd.dtypes import dtypes
 from autograd.tensor import Tensor
 
 def mnist():
     base_url = "https://raw.githubusercontent.com/fgnt/mnist/master/"
-    def _mnist(file) -> Tensor: return Tensor.from_url(base_url+file)
+    def _mnist(file) -> Tensor: return Tensor.from_url(base_url+file, dtype=dtypes.uint8)
     return _mnist("train-images-idx3-ubyte.gz")[0x10:].reshape((-1,1,28,28)), _mnist("train-labels-idx1-ubyte.gz")[8:], _mnist("t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1,1,28,28)), _mnist("t10k-labels-idx1-ubyte.gz")[8:]
