7bdf05b003fdf8d11bc57f99ddf580a7339c6ece
acrusedev <nominos09@gmail.com>
2026-01-13 13:45:24 +0100

    refactor: fix typo

--- autograd/device.py
+++ autograd/device.py
@@ -5,7 +5,7 @@ available_devices = ["CPU"]
 class _Device:
     """
     Device is where Tensors are stored and compute is run.
-    We will autodetect the best device on your system and makes it the default.
+    We will autodetect the best device on your system and make it the default.
     For now tho we will use CPU
     """
 
