b8b486827669c98c3b5eb06d36be842fbfcc08e6
acrusedev <nominos09@gmail.com>
2026-04-29 09:35:18 +0200

    add: helper methods as_mut_ptr and allocate

--- src/bin/stub_gen.rs
+++
@@ -1,7 +0,0 @@
-use pyo3_stub_gen::Result;
-
-fn main() -> Result<()> {
-    let stub = autograd_core::stub_info()?;
-    stub.generate()?;
-    Ok(())
-}
--- src/storage.rs
+++ src/storage.rs
@@ -9,7 +9,15 @@ impl Storage {
     pub fn as_ptr(&self) -> *const u8 {
         self.data.as_ptr()
     }
+    pub fn as_mut_ptr(&mut self) -> *mut u8 {
+       self.data.as_mut_ptr()
+    }
     pub fn from_slice(s: &[u8]) -> Storage {
         Storage { data: s.to_owned() }
+    }
+    pub fn allocate(nbytes: usize) -> Storage {
+        Self {
+            data: vec![0u8; nbytes],
+        }
     }
 }
