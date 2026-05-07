55a5e69811018bcd0469c6402f1b0f1eb2955dda
acrusedev <nominos09@gmail.com>
2026-05-07 09:33:39 +0200

    support more data types for adding tensors

--- src/ops/ops.rs
+++ src/ops/ops.rs
@@ -1,4 +1,5 @@
-use core::f64;
+use std::ops::Add;
+use std::process::Output;
 
 use crate::buffer::Buffer;
 use crate::dtype::DType;
@@ -6,6 +7,46 @@ use crate::helpers::{calc_strides, get_coords};
 use crate::storage::Storage;
 use pyo3::exceptions::{PyNotImplementedError, PyValueError};
 use pyo3::{PyRef, PyResult, pyfunction};
+
+fn generic_add_tensors<T>(numel: usize, tensor_a: PyRef<Buffer>, tensor_b: PyRef<Buffer>) -> Buffer
+where
+    T: Add<Output = T> + Copy,
+{
+    let nbytes = numel * std::mem::size_of::<T>();
+    let mut output = Storage::allocate(nbytes);
+    unsafe {
+        let tensor_a_slice = std::slice::from_raw_parts(tensor_a.as_ptr() as *const T, numel);
+        let tensor_b_slice = std::slice::from_raw_parts(tensor_b.as_ptr() as *const T, numel);
+        let output_slice = std::slice::from_raw_parts_mut(output.as_mut_ptr() as *mut T, numel);
+
+        let itemsize = std::mem::size_of::<T>() as isize;
+        let out_strides = calc_strides(&tensor_a.shape, std::mem::size_of::<T>() as isize);
+
+        for linear_index in 0..numel {
+            let coords = get_coords(&tensor_a.shape, linear_index);
+
+            let mut a_sum = 0;
+            let mut b_sum = 0;
+            let mut out_sum = 0;
+            for i in 0..coords.len() {
+                a_sum += tensor_a.strides[i] * coords[i];
+                b_sum += tensor_b.strides[i] * coords[i];
+                out_sum += out_strides[i] * coords[i];
+            }
+            let a_idx = (a_sum / itemsize) as usize;
+            let b_idx = (b_sum / itemsize) as usize;
+            let out_idx = (out_sum / itemsize) as usize;
+            output_slice[out_idx] = tensor_a_slice[a_idx] + tensor_b_slice[b_idx];
+        }
+
+        Buffer {
+            data: output,
+            shape: tensor_a.shape.to_owned(),
+            strides: out_strides,
+            dtype: tensor_a.dtype.to_owned(),
+        }
+    }
+}
 
 #[pyfunction]
 pub fn add_tensors(a: PyRef<Buffer>, b: PyRef<Buffer>) -> PyResult<Buffer> {
@@ -20,94 +61,12 @@ pub fn add_tensors(a: PyRef<Buffer>, b: PyRef<Buffer>) -> PyResult<Buffer> {
     let numel = a.shape.iter().map(|n| *n as usize).product::<usize>();
 
     match a.dtype {
-        DType::Int32 => {
-            let nbytes = numel * std::mem::size_of::<i32>();
-            if a.data.len() != nbytes || b.data.len() != nbytes {
-                return Err(PyValueError::new_err(
-                    "buffer byte length does not match shape and dtype",
-                ));
-            }
-
-            let mut output = Storage::allocate(nbytes);
-
-            unsafe {
-                let a_slice = std::slice::from_raw_parts(a.data.as_ptr() as *const i32, numel);
-                let b_slice = std::slice::from_raw_parts(b.data.as_ptr() as *const i32, numel);
-                let out_slice =
-                    std::slice::from_raw_parts_mut(output.as_mut_ptr() as *mut i32, numel);
-
-                let itemsize = std::mem::size_of::<i32>() as isize;
-                let out_strides = calc_strides(&a.shape, DType::get_byte_size(&DType::Int32));
-
-                for linear_index in 0..numel {
-                    let coords = get_coords(&a.shape, linear_index);
-
-                    let mut a_sum = 0;
-                    let mut b_sum = 0;
-                    let mut out_sum = 0;
-                    for i in 0..coords.len() {
-                        a_sum += a.strides[i] * coords[i];
-                        b_sum += b.strides[i] * coords[i];
-                        out_sum += out_strides[i] * coords[i];
-                    }
-                    let a_idx = (a_sum / itemsize) as usize;
-                    let b_idx = (b_sum / itemsize) as usize;
-                    let out_idx = (out_sum / itemsize) as usize;
-                    out_slice[out_idx] = a_slice[a_idx] + b_slice[b_idx];
-                }
-
-                Ok(Buffer {
-                    data: output,
-                    shape: a.shape.to_owned(),
-                    strides: out_strides,
-                    dtype: a.dtype.to_owned(),
-                })
-            }
-        }
-        DType::Float64 => {
-            let nbytes = numel * std::mem::size_of::<f64>();
-            if a.data.len() != nbytes || b.data.len() != nbytes {
-                return Err(PyValueError::new_err(
-                    "buffer byte length does not match shape and dtype",
-                ));
-            }
-
-            let mut output = Storage::allocate(nbytes);
-
-            unsafe {
-                let a_slice = std::slice::from_raw_parts(a.data.as_ptr() as *const f64, numel);
-                let b_slice = std::slice::from_raw_parts(b.data.as_ptr() as *const f64, numel);
-                let out_slice =
-                    std::slice::from_raw_parts_mut(output.as_mut_ptr() as *mut f64, numel);
-
-                let itemsize = std::mem::size_of::<f64>() as isize;
-                let out_strides = calc_strides(&a.shape, DType::get_byte_size(&DType::Float64));
-
-                for linear_index in 0..numel {
-                    let coords = get_coords(&a.shape, linear_index);
-
-                    let mut a_sum = 0;
-                    let mut b_sum = 0;
-                    let mut out_sum = 0;
-                    for i in 0..coords.len() {
-                        a_sum += a.strides[i] * coords[i];
-                        b_sum += b.strides[i] * coords[i];
-                        out_sum += out_strides[i] * coords[i];
-                    }
-                    let a_idx = (a_sum / itemsize) as usize;
-                    let b_idx = (b_sum / itemsize) as usize;
-                    let out_idx = (out_sum / itemsize) as usize;
-                    out_slice[out_idx] = a_slice[a_idx] + b_slice[b_idx];
-                }
-
-                Ok(Buffer {
-                    data: output,
-                    shape: a.shape.to_owned(),
-                    strides: out_strides,
-                    dtype: a.dtype.to_owned(),
-                })
-            }
-        }
+        DType::Int8 => Ok(generic_add_tensors::<i8>(numel, a, b)),
+        DType::Int16 => Ok(generic_add_tensors::<i16>(numel, a, b)),
+        DType::Int32 => Ok(generic_add_tensors::<i32>(numel, a, b)),
+        DType::Int64 => Ok(generic_add_tensors::<i64>(numel, a, b)),
+        DType::Float32 => Ok(generic_add_tensors::<f32>(numel, a, b)),
+        DType::Float64 => Ok(generic_add_tensors::<f64>(numel, a, b)),
         _ => Err(PyNotImplementedError::new_err(
             "add is currently implemented only for int32 buffers",
         )),
