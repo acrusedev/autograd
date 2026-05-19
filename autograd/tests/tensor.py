d37d094a80a02d977440e9edc266ae750c653533
acrusedev <nominos09@gmail.com>
2026-05-19 11:49:01 +0200

    tests: add test for casting tensors

--- autograd/tests/tensor.py
+++ autograd/tests/tensor.py
@@ -1,12 +1,20 @@
+from autograd.dtypes import dtypes
 import unittest
 from autograd import Tensor
 
 class TestTensor(unittest.TestCase):
-    def test_can_add_tensors(self):
-        a = Tensor([1,2,3,4,5,6,7,8,9])
-        b = Tensor([9,8,7,6,5,4,3,2,1])
-        e = a+b
-        e.realize()
-        res = Tensor([10,10,10,10,10,10,10,10,10]).realize()
-        self.assertEqual(e.shape,a.shape)
-        self.assertEqual(str(e),str(res))
+  def test_can_add_tensors(self):
+    a = Tensor([1,2,3,4,5,6,7,8,9])
+    b = Tensor([9,8,7,6,5,4,3,2,1])
+    e = a+b
+    e.realize()
+    res = Tensor([10,10,10,10,10,10,10,10,10]).realize()
+    self.assertEqual(str(e),str(res))
+
+  def test_cast_tensors(self):
+    a = Tensor([1,2,3,4,5,6,7,8,9], dtype='int32')
+    self.assertEqual(a.dtype, dtypes.int32)
+    b = Tensor([1,2,3,4,5,6,7,8,9], dtype='int64')
+    self.assertEqual(b.dtype, dtypes.int64)
+    e = (a+b).realize()
+    self.assertEqual(e.dtype, dtypes.int64)
