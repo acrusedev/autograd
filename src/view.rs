c37be22d8de315e6fa487ff792b0a88aacd94c3b
acrusedev <nominos09@gmail.com>
2026-06-24 11:33:44 +0200

    fix: view should expose shape and strides as tuples

--- src/view.rs
+++ src/view.rs
@@ -1,4 +1,4 @@
-use pyo3::{Bound, PyAny, PyResult, pyclass, pymethods};
+use pyo3::{Bound, PyAny, PyResult, Python, pyclass, pymethods, types::PyTuple};
 use pyo3_stub_gen::derive::{gen_stub_pyclass, gen_stub_pymethods};
 
 #[gen_stub_pyclass]
@@ -24,5 +24,23 @@ impl View {
             strides,
             offset,
         }
+    }
+}
+
+#[pymethods]
+impl View {
+    #[getter]
+    fn shape<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyTuple>> {
+        PyTuple::new(py, self.shape.iter().copied())
+    }
+
+    #[getter]
+    fn strides<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyTuple>> {
+        PyTuple::new(py, self.strides.iter().copied())
+    }
+
+    #[getter]
+    fn offset(&self) -> isize {
+        self.offset
     }
 }
